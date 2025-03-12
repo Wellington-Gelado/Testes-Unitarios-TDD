c29e90216cdf2c2939a60eef84c17d3ef19cf44b
Wellington <weellingtonsantos1996@gmail.com>
2025-03-12 15:31:52 -0300

    Testes sem o uso do TDD.

---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,61 @@
+package br.com.alura.tdd.service;
+
+import br.com.alura.tdd.modelo.Funcionario;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BonusServiceTest {
+
+    @Test
+    @Order(1)
+    void bonusDeveriaSerZeroParaFuncionarioComSalarioMuitoAlto(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.
+                        calcularBonus(
+                                new Funcionario(
+                                        "Wellington",
+                                         LocalDate.now(),
+                                         new BigDecimal("25000")
+                                )
+                        );
+
+        assertEquals(new BigDecimal("0.00"), bonus);
+    }
+
+    @Test
+    @Order(2)
+    void bonusDeveriaSerDezPorCentoDoSalario(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.
+                calcularBonus(
+                        new Funcionario(
+                                "Wellington",
+                                LocalDate.now(),
+                                new BigDecimal("9000")
+                        )
+                );
+
+        assertEquals(new BigDecimal("900.00"), bonus);
+    }
+
+    @Test
+    @Order(3)
+    void bonusDeveriaSerDezPorCentoParaSalarioDeExatamenteDezMilReais(){
+        BonusService service = new BonusService();
+        BigDecimal bonus = service.
+                calcularBonus(
+                        new Funcionario(
+                                "WellingtonS",
+                                LocalDate.now(),
+                                new BigDecimal("10000")
+                        )
+                );
+
+        assertEquals(new BigDecimal("1000.00"), bonus);
+    }
+}
